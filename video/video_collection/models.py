6634494b0f8dd5611c85786630efafa2cc4131a5
rwalk651 <user@example.com>
2024-11-20 00:02:29 -0500

    video model added

--- video/video_collection/models.py
+++ video/video_collection/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    # video name
+    name = models.CharField(max_length=200)
+    # url of video
+    url = models.CharField(max_length=400)
+    # notes about video, can be empty or blank - not required to submit
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        # returns string of object - notes restricted to first 200 characters
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
