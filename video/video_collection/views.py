9ff9c252788b16792d96ea05117f49f38be94cf4
rwalk651 <user@example.com>
2024-11-20 01:09:58 -0500

    warning message if invalid data in video form or video saved message if valid

--- video/video_collection/views.py
+++ video/video_collection/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import VideoForm
+from django.contrib import messages
 
 # homepage view
 def home(request):
@@ -9,6 +10,18 @@ def home(request):
 
 # to add new video
 def add(request):
+    # get request to get page or post request to create a new video
+    if request.method == 'POST':
+        new_video_form = VideoForm(request.post)
+        if new_video_form.is_valid():
+            # if form data is valid - save to database
+            new_video_form.save()
+            messages.info(request, 'New video saved')
+        else:
+            messages.warning(request, 'Please check data entered')
+            # return same form with input if a validation error occurred
+            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
     # requests video form from for model
     new_video_form = VideoForm()
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
