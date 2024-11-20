2f9cf7684034ceed9202e986c5c86d1b9295cf54
rwalk651 <user@example.com>
2024-11-20 00:33:01 -0500

    created base html and style css

--- video/video_collection/views.py
+++ video/video_collection/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # homepage view
 def home(request):
-    return render(request, 'video_collection/home.html')
+    # make app name variable to display on homepage
+    app_name = 'Music Videos'
+    return render(request, 'video_collection/home.html', {'app_name': app_name})
